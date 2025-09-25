da67ea572d2109f17ba8bb3d1aceb50e078aeece
Oleg Zeltser <oleg@zeltsergroup.com>
2025-09-24 20:33:15 -0400

    ai suggestions

--- relay.py
+++ relay.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
 import time
-
 import automationhat
 
 if automationhat.is_automation_hat():
-    automationhat.light.power.write(1)
-    automationhat.relay.one.on()
-    time.sleep(5)
-    automationhat.relay.one.off()
+    try:
+        automationhat.light.power.write(1)
+        automationhat.relay.one.on()
+        time.sleep(5)
+    finally:
+        automationhat.relay.one.off()
+        automationhat.light.power.write(0)
